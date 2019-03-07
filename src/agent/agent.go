6dcc1c2c62061efab9a4940daeb0f135502b78c2
berkaykan <brkykan@gmail.com>
2019-03-07 11:58:32 +0100

    Create first iteration of agent package

--- src/agent/agent.go
+++ src/agent/agent.go
@@ -1,1 +1,31 @@
 package agent
+
+import (
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type agent struct {
+	client http.Client
+}
+
+type Requester interface {
+	PerformGetRequest(url *url.URL) (*http.Response, error)
+}
+
+func (a *agent) PerformGetRequest(url *url.URL) (*http.Response, error) {
+	if url.Scheme == "" {
+		url.Scheme = "https"
+	}
+	resp, err := a.client.Get(url.String())
+	return resp, err
+}
+
+func NewAgent() Requester {
+	return &agent{
+		client: http.Client{
+			Timeout: 10 * time.Second, // a sensible time period to deem it as timed out
+		},
+	}
+}
