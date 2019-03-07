0e661a961ef580c8bbe56fb4f9a3f7fac04ce2f5
berkaykan <brkykan@gmail.com>
2019-03-07 12:00:39 +0100

    Add CL flags and arguments Implement config setup in main.go

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"config"
+	"coordinator"
+	"flag"
+)
+
+func main() {
+
+	parallel := flag.Int("parallel", 10, "Allowed parallel requests limit")
+	flag.Parse()
+	args := flag.Args()
+
+	configuration := config.Setup(args, *parallel)
+	coordinator.Run(configuration)
+}
