dfb03302b44632617ff9e41126ee012080b6ce47
berkaykan <brkykan@gmail.com>
2019-03-08 01:38:43 +0100

    Set limit to maximum 10

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ func main() {
 	parallel := flag.Int("parallel", 10, "Allowed parallel requests limit")
 	flag.Parse()
 	args := flag.Args()
+	limit := *parallel
+	if limit > 10 {
+		limit = 10
+	}
 
-	configuration := config.Setup(args, *parallel)
+	configuration := config.Setup(args, limit)
 	coordinator.Run(configuration)
 }
