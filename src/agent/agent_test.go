c10eec6b251f7fb7d8fc6f937c96290ef7bfa5be
berkaykan <brkykan@gmail.com>
2019-03-08 01:39:50 +0100

    Implement agent tests

--- src/agent/agent_test.go
+++ src/agent/agent_test.go
@@ -1,74 +1,52 @@
 package agent
 
 import (
-	"agent/errors"
-	"io/ioutil"
-	"net/http"
-	"strings"
 	"testing"
 )
 
 func TestAgent(t *testing.T) {
 
 	testCases := []struct {
-		Description      string
-		GivenURL         string
-		ExpectedError    error
-		ExpectedResponse *http.Response
+		Description   string
+		GivenURL      string
+		ExpectedError bool
 	}{
 		{
 			Description:   "happy path",
 			GivenURL:      "https://httpbin.org/base64/SFRUUEJJTiBpcyBhd2Vzb21l",
-			ExpectedError: nil,
-			ExpectedResponse: &http.Response{
-				Body: ioutil.NopCloser(strings.NewReader("HTTPBIN is awesome")),
-			},
+			ExpectedError: false,
 		},
 		{
 			Description:   "happy path/no scheme",
 			GivenURL:      "httpbin.org/base64/SFRUUEJJTiBpcyBhd2Vzb21l",
-			ExpectedError: nil,
-			ExpectedResponse: &http.Response{
-				Body: ioutil.NopCloser(strings.NewReader("HTTPBIN is awesome")),
-			},
+			ExpectedError: false,
 		},
 		{
-			Description:      "failed path/invalid url",
-			GivenURL:         "invalidurl\n",
-			ExpectedError:    errors.ErrInvalidURL,
-			ExpectedResponse: nil,
+			Description:   "failed path/invalid url",
+			GivenURL:      "invalid\nURL",
+			ExpectedError: true,
 		},
 		{
-			Description:      "failed path/get failed",
-			GivenURL:         "localhost",
-			ExpectedError:    errors.ErrConnectionFailed,
-			ExpectedResponse: nil,
+			Description:   "failed path/get failed",
+			GivenURL:      "localhost",
+			ExpectedError: true,
 		},
 		{
-			Description:      "failed path/nil url",
-			GivenURL:         "",
-			ExpectedError:    errors.ErrConnectionFailed,
-			ExpectedResponse: nil,
+			Description:   "failed path/nil url",
+			GivenURL:      "",
+			ExpectedError: true,
 		},
 	}
 
 	for _, testCase := range testCases {
 		agent := NewAgent()
 		t.Run(testCase.Description, func(t *testing.T) {
-			resp, err := agent.MakeRequest(testCase.GivenURL)
+			_, err := agent.MakeRequest(testCase.GivenURL)
 
-			if testCase.ExpectedError == nil && err != nil {
+			if !testCase.ExpectedError && err != nil {
 				t.Error("Assertion fails, actual error is not nil")
-			}
-			if err != testCase.ExpectedError {
-				t.Errorf("Assertion fails, expected error: %v actual error: %v", testCase.ExpectedError, err)
-			}
-			if testCase.ExpectedResponse != nil && resp == nil {
-				t.Errorf("Assertion fails, expected response: %v actual response: %v", testCase.ExpectedResponse, nil)
-			}
-
-			if testCase.ExpectedResponse != nil && resp != nil && testCase.ExpectedResponse.Body != resp.Body {
-				t.Errorf("Assertion fails, expected body: %v actual body: %v", testCase.ExpectedResponse, resp.Body)
+			} else if testCase.ExpectedError && err == nil {
+				t.Error("Assertion fails, actual error is nil")
 			}
 		})
 	}
